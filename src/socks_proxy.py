048cc654d0b5c51efccf2e5512d1f4c2a9b7b659
ctz <jpixton@gmail.com>
2013-03-22 16:56:51 +0000

    print ciphersuites, tidying

--- src/socks_proxy.py
+++ src/socks_proxy.py
@@ -10,21 +10,6 @@ MAX = 8192
 
 # transform functions take the from-local and from-remote data,
 # and return the to-local and to-remote data, plus the next transform
-"""
-while True:
-
-m = Message.read(self.rfile)
-self.wfile.write(bytes(build_fatal_alert(AlertDescription.HandshakeFailure)))
-self.rfile.close()
-self.wfile.close()
-
-assert m.type == ContentType.Handshake
-print('TLS version:', ProtocolVersion.tostring(m.version))
-assert m.body.type == HandshakeType.ClientHello
-for cs in m.body.body.ciphersuites:
-    print(CipherSuite.lookup(cs))
-print('---')
-"""
 
 def tls_mitm_transform(from_front, from_back):
     if from_front:
@@ -32,6 +17,7 @@ def tls_mitm_transform(from_front, from_back):
         if m.type == ContentType.Handshake and m.body.type == HandshakeType.ClientHello:
             print('message version:', ProtocolVersion.tostring(m.version))
             print('clienthello max version:', ProtocolVersion.tostring(m.body.body.version))
+            print('ciphersuites:', [CipherSuite.lookup(cs) for cs in m.body.body.ciphersuites])
             if m.version >= ProtocolVersion.TLSv1_0:
                 print('rejecting handshake to encourage downgrade')
                 return bytes(build_fatal_alert(AlertDescription.HandshakeFailure)), None, tls_mitm_transform
@@ -48,25 +34,28 @@ def echo_transform(from_front, from_back):
     return from_front, from_back, echo_transform
 
 class socks_handler(socketserver.BaseRequestHandler):
-    def handle(self):
-        print('socks client connected from', self.client_address)
-        
+    def handle_socks_setup(self):
         req = self.request.recv(9)
         ver, cmd, port, ip, nul = req[0], req[1], req[2:4], req[4:8], req[8]
         assert ver == 0x04
         assert cmd == 0x01
         assert nul == 0x00
         
-        ip = '{0}.{1}.{2}.{3}'.format(*ip)
-        port = port[0] << 8 | port[1]
+        self.backend_ip = '{0}.{1}.{2}.{3}'.format(*ip)
+        self.backend_port = port[0] << 8 | port[1]
         
-        print('connect to {0}:{1}'.format(ip, port))
-        backend = socket.create_connection((ip, port))
+        print('connect to {0}:{1}'.format(self.backend_ip, self.backend_port))
+        self.backend = socket.create_connection((self.backend_ip, self.backend_port))
         print('connected')
 
         self.request.sendall(bytes([0, 0x5a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]))
 
-        if port == 443 or port == 4433:
+    def handle(self):
+        print('socks client connected from', self.client_address)
+        
+        self.handle_socks_setup()
+
+        if self.backend_port in (443, 4433):
             transform = tls_mitm_transform
         else:
             transform = null_transform
@@ -78,21 +67,21 @@ class socks_handler(socketserver.BaseRequestHandler):
                 self.request.sendall(to_front)
             if to_back:
                 print('->', len(to_back))
-                backend.sendall(to_back)
+                self.backend.sendall(to_back)
             return transform
 
         try:
             while True:
-                r, w, x = select.select([backend, self.request],
+                r, w, x = select.select([self.backend, self.request],
                                         [],
-                                        [backend, self.request],
+                                        [self.backend, self.request],
                                         1)
 
                 if x:
                     raise IOError('socket error: {0}'.format(x))
 
-                if backend in r:
-                    packet = backend.recv(MAX)
+                if self.backend in r:
+                    packet = self.backend.recv(MAX)
                     if len(packet) == 0:
                         print('eof from back')
                         break
@@ -110,7 +99,7 @@ class socks_handler(socketserver.BaseRequestHandler):
             os._exit(1)
         finally:
             print('closing')
-            backend.close()
+            self.backend.close()
             self.request.close()
         
 class RebindTCP(socketserver.ThreadingMixIn, socketserver.TCPServer):
