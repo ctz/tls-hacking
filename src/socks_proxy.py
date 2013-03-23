faaae71473dd8e639c4921ae6000ba5ba1bd230b
ctz <jpixton@gmail.com>
2013-03-23 22:15:16 +0000

    clean up logging

--- src/socks_proxy.py
+++ src/socks_proxy.py
@@ -4,6 +4,7 @@ import select
 import os
 import io
 import sys
+import tempfile
 
 from tls import Message, build_fatal_alert, AlertDescription
 from tls import ContentType, HandshakeType, CipherSuite, ProtocolVersion
@@ -11,22 +12,26 @@ from tls import ContentType, HandshakeType, CipherSuite, ProtocolVersion
 MAX = 8192
 ENABLE_DOWNGRADE = True
 
-def dump(why, bb):
-    #print(why, len(bb), '::', ''.join(['{0:02x}'.format(x) for x in bb]))
-    pass
-
 class socks_handler(socketserver.BaseRequestHandler):
     def mustrecv(self, n, why = 'data'):
         x = self.request.recv(n)
         if len(x) != n:
             raise IOError('short ' + why)
         return x
+
+    def log(self, *args, **kwargs):
+        print(file = self.logf, *args, **kwargs)
             
+    def dump(self, why, bb):
+        self.log(why, len(bb), '::', ''.join(['{0:02x}'.format(x) for x in bb]))
+    
     def connect_backend(self, ip_be, port_be):
         self.backend_ip = '{0}.{1}.{2}.{3}'.format(*ip_be)
         self.backend_port = port_be[0] << 8 | port_be[1]
-        
+
+        self.log('connecting to', self.backend_ip, 'port', self.backend_port)
         self.backend = socket.create_connection((self.backend_ip, self.backend_port))
+        self.log('connected')
 
     def handle_socks_setup(self):
         ver = self.mustrecv(1, 'socks version')[0]
@@ -74,27 +79,27 @@ class socks_handler(socketserver.BaseRequestHandler):
         self.request.sendall(bytes([0x05, 0x00, 0x00, 0x01] + list(ip) + list(port)))
 
     def tls_incoming(self, m):
-        #print('incoming from remote', m)
+        self.log('incoming from remote', m)
         
         if m.type == ContentType.ChangeCipherSpec:
             self.seen_changecipherspec = True
 
         if m.type == ContentType.Handshake and not m.opaque and m.body.type == HandshakeType.ServerHello:
-            print('server selected', CipherSuite.tostring(m.body.body.ciphersuite))
+            self.log('server selected', CipherSuite.tostring(m.body.body.ciphersuite))
 
         self.request.sendall(bytes(m))
 
     def tls_outgoing(self, m):
-        #print('outgoing from local', m)
+        self.log('outgoing from local', m)
         
         if m.type == ContentType.ChangeCipherSpec:
             self.seen_changecipherspec = True
             
         if m.type == ContentType.Handshake and m.version >= ProtocolVersion.TLSv1_0 and ENABLE_DOWNGRADE:
-            print('sabotaging >= TLS1.0')
-            print('original ciphersuites were:')
+            self.log('sabotaging >= TLS1.0')
+            self.log('original ciphersuites were:')
             for cs in m.body.body.ciphersuites:
-                print(' ', CipherSuite.tostring(cs))
+                self.log(' ', CipherSuite.tostring(cs))
             self.request.sendall(bytes(build_fatal_alert(AlertDescription.HandshakeFailure)))
             return
         
@@ -120,8 +125,11 @@ class socks_handler(socketserver.BaseRequestHandler):
             self.echo(self.oubuf, self.backend)
             self.echo(self.inbuf, self.request)
 
-    def handle(self):        
+    def handle(self):
+        logfd, logfile = tempfile.mkstemp(dir = 'tmp/', suffix = '.txt', prefix = 'proxy')
+        self.logf = os.fdopen(logfd, 'w')
         self.handle_socks_setup()
+
         self.inbuf = []
         self.oubuf = []
         self.seen_changecipherspec = False
@@ -142,7 +150,7 @@ class socks_handler(socketserver.BaseRequestHandler):
                     packet = bytes(self.backend.recv(MAX))
                     if len(packet) == 0:
                         break
-                    dump('incoming', packet)
+                    self.dump('incoming', packet)
                     self.inbuf.extend(packet)
                     self.check_bufs()
                 
@@ -150,7 +158,7 @@ class socks_handler(socketserver.BaseRequestHandler):
                     packet = bytes(self.request.recv(MAX))
                     if len(packet) == 0:
                         break
-                    dump('outgoing', packet)
+                    self.dump('outgoing', packet)
                     self.oubuf.extend(packet)
                     self.check_bufs()
                 
@@ -159,6 +167,7 @@ class socks_handler(socketserver.BaseRequestHandler):
         finally:
             self.backend.close()
             self.request.close()
+            self.logf.close()
 
 class RebindTCP(socketserver.ThreadingMixIn, socketserver.TCPServer):
     def __init__(self, *args, **kwargs):
